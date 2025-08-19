3f104ef7245b538fe0ac0417f1d98eba5e8b0140
Markus Hergenröder <markus@coding4glory.net>
2025-08-19 19:47:37 +0200

    removed redundant code, added unload function

--- lua/tiny-windows/commands.lua
+++ lua/tiny-windows/commands.lua
@@ -27,15 +27,10 @@ return {
             core.buffer_close,
             { desc = "close buffer but try to keep window", bang = true }
         )
-
-        if opts.try_keep_open then
-            vim.api.create_autocmd(
-                'WinClosed',
-                {
-                    group = vim.api.nvim_create_augroup('tiny_keep_window', { clear = true }),
-                    callback = core.keep_open_buffer,
-                    desc = "opens new window if last window is closed but editor buffers available",
-                })
-        end
     end,
+    unload = function()
+        for cmd, _ in pairs(vim.api.nvim_get_commands({builtin = false})) do
+            if cmd == 'Bc' then vim.api.nvim_del_user_command('Bc') end
+        end
+    end
 }
